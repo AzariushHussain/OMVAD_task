87c9ca86de97dd3af898c633f8e685293644b08e
Azariush Hussain <azariushhussain@gmail.com>
2025-05-07 15:10:07 +0530

    updated login url

--- frontend/src/components/AuthForm.jsx
+++ frontend/src/components/AuthForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const API_URL = `${import.meta.env.VITE_API_URL}/auth`;
+const API_URL = `${import.meta.env.VITE_API_URL}auth`;
 
 const AuthForm = ({ setToken }) => {
     const [email, setEmail] = useState('');
